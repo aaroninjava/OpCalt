7f22cd3cb95ea46796d1ec921cbf7f022675b5e1
aarontsai <aaroninpcschool241@gmail.com>
2018-02-06 03:18:29 +0800

    BSa-DSa-outP

---
+++ app/src/main/java/com/example/aaroninjava/opcalt/DSa.java
@@ -0,0 +1,45 @@
+package com.example.aaroninjava.opcalt;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by AaroninJava on 2018/2/6.
+ */
+
+public class DSa {
+
+
+    String ts;
+    double ds;
+    DecimalFormat df = new DecimalFormat("##.0000");
+
+    DSa(){}
+
+    DSa(String ts)
+    { this.ts=ts;}
+
+    DSa(Double ds)
+    {this.ds=ds;}
+
+    DSa(String ts,Double ds)
+    {
+        this.ts=ts;
+        this.ds=ds;
+    }
+
+    public String getTs( double ds) {
+        this.ds=ds;
+        String ts1;
+        ts1=String.valueOf( Double.parseDouble(df.format(ds)) );
+        return ts1;
+    }
+
+    public double getDs(String ts) {
+        double ds2;
+        ds2=Double.parseDouble(df.format(Double.parseDouble(ts)));
+        return ds2;
+    }
+
+
+
+}
--- app/src/main/java/com/example/aaroninjava/opcalt/outP.java
+++ app/src/main/java/com/example/aaroninjava/opcalt/outP.java
@@ -2,12 +2,61 @@ package com.example.aaroninjava.opcalt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class outP extends AppCompatActivity {
+
+    TextView tv;
+    DSa dsa=new DSa();
+    double Pr,S,K,T,sigma,r,y;
+    String CP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_out_p);
+
+        String x1;
+
+        CP="C";
+        Pr=1.732;
+        Pr=0.84;
+        S=100;
+        K=110;
+        T=0.1;
+        r=0.01;
+        y=0;
+        sigma=0.3;
+
+        BSa b = new BSa(CP,Pr,S,K,T,sigma,r,y);
+//DSa dsa=new DSa();
+        x1=dsa.getTs(b.iv1());
+//System.out.println("call"+b.call());
+        //x1=dsa.getTs(b.call());
+        System.out.println("Call = "+x1);
+//x1="123";
+        tv = findViewById(R.id.tvBS);
+        tv.setText(x1);
+
     }
+
+
+
+
+
+    public void click1(View v)
+    {
+        String x1;
+        tv = findViewById(R.id.tvBS);
+
+
+
+        //Date date=new Date();
+        //System.out.println(date.toString());
+        //tv.setText("123");
+        //tv.setText(date.toString());
+        //tv.setText(x1);
+    }
+
 }
