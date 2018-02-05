32d55dc19d7425c6fbf6df9c8196dd04aaf21076
aarontsai <aaroninpcschool241@gmail.com>
2018-02-06 03:01:47 +0800

    Laout-outP

--- app/src/main/java/com/example/aaroninjava/opcalt/inP.java
+++ app/src/main/java/com/example/aaroninjava/opcalt/inP.java
@@ -1,7 +1,9 @@
 package com.example.aaroninjava.opcalt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class inP extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class inP extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_p);
+
+
     }
+
+
+    public void clickinP(View v) {
+        Intent it = new Intent(inP.this, outP.class);
+        startActivity(it);
+    }
+
+
+
 }
